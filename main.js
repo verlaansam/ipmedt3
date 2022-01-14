9e5c1b1be78a77d9f33db024e214afbd72a41876
sam <verlaansam@gmail.com>
2022-01-14 12:45:04 +0100

    functie spawnbox and spawnquizbox uitbreiden

--- main.js
+++ main.js
@@ -1,95 +1,84 @@
 window.onload = (event) =>{
-  
+  let lost = false;
   var x;
   let point = 0;
   var score = document.getElementById("score")
 
-
-  function spawnBox(x){
+  //spawns regular blocks
+  function spawnBox(x, y, z, w, h, d, c, s){
     var box = document.createElement("a-box")
     box.setAttribute("point", "null")
     box.setAttribute("geometry","primitive: box")
     box.setAttribute("ammo-body", "type: dynamic; emitCollisionEvents: true;")
     box.setAttribute("ammo-shape", "type: box")
-    box.setAttribute("width", "1")
-    box.setAttribute("height", "1")
-    box.setAttribute("depth", "2")
-    box.setAttribute("position", `${x} 90 5`)
-    box.setAttribute("color", "green")
-    document.querySelector("a-scene").appendChild(box)
+    box.setAttribute("width", `${w}`)
+    box.setAttribute("height", `${h}`)
+    box.setAttribute("depth", `${d}`)
+    box.setAttribute("position", `${x} ${y} ${z}`)
+    box.setAttribute("color", `${c}`)
+    document.querySelector(`${s}`).appendChild(box)
   }
 
-  function spawnBoxQuiz(x){
+  //spawns special quiz block
+  function spawnBoxQuiz(x, y, z, w, h, d, c, s){
     var box = document.createElement("a-box")
+    box.setAttribute("quiz", "null")
     box.setAttribute("geometry","primitive: box")
     box.setAttribute("ammo-body", "type: dynamic; emitCollisionEvents: true;")
     box.setAttribute("ammo-shape", "type: box")
-    box.setAttribute("width", "1")
-    box.setAttribute("height", "1")
-    box.setAttribute("depth", "2")
-    box.setAttribute("position", `${x} 90 5`)
-    box.setAttribute("color", "orange")
-    document.querySelector("a-scene").appendChild(box)
+    box.setAttribute("width", `${w}`)
+    box.setAttribute("height", `${h}`)
+    box.setAttribute("depth", `${d}`)
+    box.setAttribute("position", `${x} ${y} ${z}`)
+    box.setAttribute("color", `${c}`)
+    document.querySelector(`${s}`).appendChild(box)
   }
 
-  function sequenceZero(){
-    i = Math.floor(Math.random() * 8);
-    spawnBoxQuiz(i)
-  }
-
-  function sequenceOne(){
-    spawnBox(0)
-    spawnBox(3)
-    spawnBox(6)
-  }
-
-  function sequenceTwo(){
-    spawnBox(1)
-    spawnBox(4)
-    spawnBox(7)
-  }
-
-  function sequenceThree(){
-    j = Math.floor(Math.random() * 8);
-    spawnBox(j)
-  }
-
-  function sequenceFour(){
-    j = Math.floor(Math.random() * 6) + 2;
-    spawnBox(j)
-    spawnBox(j+2)
-  }
-
+  //choses random spwan patern to spawn blocks in
   function randomSequence(){
-    
     i = Math.floor(Math.random() * 5);
-     switch(i){
+    switch(i){
       case 0:
-         sequenceZero();
-         break;
+        j = Math.floor(Math.random() * 8);
+        spawnBoxQuiz(j, 90, 5, 1, 1, 2, "orange", "a-scene")
+        break;
       case 1:
-          sequenceOne()
-         break;
+        spawnBox(0, 90, 5, 1, 1, 2, "green", "a-scene")
+        spawnBox(3, 90, 5, 1, 1, 2, "green", "a-scene")
+        spawnBox(6, 90, 5, 1, 1, 2, "green", "a-scene")
+        break;
       case 2:
-         sequenceTwo();
-         break;
+        spawnBox(1, 90, 5, 1, 1, 2, "green", "a-scene")
+        spawnBox(4, 90, 5, 1, 1, 2, "green", "a-scene")
+        spawnBox(7, 90, 5, 1, 1, 2, "green", "a-scene")
+        break;
       case 3:
-         sequenceThree();
-         break;
+        j = Math.floor(Math.random() * 8);
+        spawnBox(j, 90, 5, 1, 1, 2, "green", "a-scene");
+        break;
       case 4:
-         sequenceFour();
-         break;
-     }
+        j = Math.floor(Math.random() * 6) + 2;
+        spawnBox(j, 90, 5, 1, 1, 2, "green", "a-scene")
+        spawnBox(j+2, 90, 5, 1, 1, 2, "green", "a-scene")
+        break;
+    }
   }
 
+  
+
+
+  //adds poiny every half second
   function pointsTime(){
     point +=1
     score.innerHTML = point
+    //timespend functie?
   }
 
-  setInterval(randomSequence, 2000);
-  setInterval(pointsTime, 500)
+  
+  
+/*a-frame components*/
 
+  //gives point when you move block away
   AFRAME.registerComponent('point', {
     init: function() {
       var el = this.el;
@@ -97,29 +86,43 @@ window.onload = (event) =>{
         point += 1;
         console.log(point)
         score.innerHTML = point
+        //blocks hit function
       });
     }
   });
   
-AFRAME.registerComponent('quiz', {
-  init: function() {
-    var el = this.el;
-    el.addEventListener("collidestart", function () {
-      console.log("quiz")
-    });
-  }
-});
 
+
+//takes care of everything
+setInterval(randomSequence, 2000);
+setInterval(pointsTime, 500)
+}
+
+//when protected block is hit
 AFRAME.registerComponent('gameover', {
   init: function() {
     var el = this.el;
     el.addEventListener("collidestart", function () {
       console.log("GameOver")
+      gameOver()
     });
   }
 });
-  
+
+function gameOver(){
+  console.log("GameOver")
+  spawnBox(0, 0, -3, 3, 1, 1, "red", "#camera")
 }
 
+//when you touch quiz block
+AFRAME.registerComponent('quiz', {
+  init: function() {
+    var el = this.el;
+    el.addEventListener("collidestart", function () {
+      console.log("quiz")
+      //quizblocks hit function
+    });
+  }
+});
 
   
