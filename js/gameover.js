b0c570b42f1066abd175a72e8cecce0c1aff9d7b
sam <verlaansam@gmail.com>
2022-01-24 10:09:47 +0100

    aanpassen van omgeving en gameover scherm

--- js/gameover.js
+++ js/gameover.js
@@ -1,13 +1,36 @@
 window.onload = (event) =>{
-    var finalscore = document.getElementById("FinalScore")
+    var places = document.getElementsByClassName("js--place")
+
     var score = localStorage.getItem('points')
-    finalscore.innerHTML = score
+    var textScore = document.createElement("a-entity")
+    textScore.setAttribute("scale","5 5 5")
+    textScore.setAttribute("position", "2.5 0 -4")
+    textScore.setAttribute("text", `value: ${score}; color:white;`)
+    document.querySelector("a-scene").appendChild(textScore)
 
-    var finaltime = document.getElementById("FinalTime")
     var time = localStorage.getItem('time')
-    finaltime.innerHTML = time
+    var textTime = document.createElement("a-entity")
+    textTime.setAttribute("scale","5 5 5")
+    textTime.setAttribute("position", "0 0 -4")
+    textTime.setAttribute("text", `value: ${time}; color:white;`)
+    document.querySelector("a-scene").appendChild(textTime)
 
-    var finalquiz = document.getElementById("FinalQuiz")
     var quiz = localStorage.getItem('quiz')
-    finalquiz.innerHTML = quiz
+    var textQuiz = document.createElement("a-entity")
+    textQuiz.setAttribute("scale","5 5 5")
+    textQuiz.setAttribute("position", "4.8 -0.2 -4")
+    textQuiz.setAttribute("text", `value: ${quiz}; color:white;`)
+    document.querySelector("a-scene").appendChild(textQuiz)
+
+
+    for (let i = 0; i < places.length; i++) {
+        console.log(places[i])
+        places[i].addEventListener('click', function(evt){
+            console.log(places[i])
+            let att = document.createAttribute("animation");
+            att.value = "property: position; easing: linear; dur: 2000; to: " + this.getAttribute('position').x + " 1.6 " + this.getAttribute('position').z;
+            camera.setAttribute('animation', att.value);
+        });
+    }
+    
 }
--- js/main.js
+++ js/main.js
@@ -132,7 +132,7 @@ window.onload = (event) =>{
 
   //takes care of everything
   setInterval(randomSequence,  2000-time);
-  setInterval(move, 10);
+  // setInterval(move, 10);
   setInterval(pointsTime, 500);
   
 
